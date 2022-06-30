5db49be418b42c7cad7826185a359d66f93ab490
Corum Gold <goldcorum@gmail.com>
2022-06-30 10:37:45 -0400

    Solve object properties -- 2nd

--- TestFirst-1-Practice/02-object-properties/object-properties.js
+++ TestFirst-1-Practice/02-object-properties/object-properties.js
@@ -1,6 +1,36 @@
 /* eslint-disable no-unused-vars */
-// solveCount = 1;
+// solveCount = 2;
 
 function setPropsOnObj(obj) {
-    
+  obj.x = 7;
+  obj.y = 8;
+  obj.onePlus = (arg) => {
+    return arg + 1;
+  };
 }
+
+function setPropsOnArr(arr) {
+  arr.hello = () => {
+    return 'Hello!';
+  };
+  arr.full = 'stack';
+  arr[0] = 5;
+  arr.twoTimes = (num) => {
+    return num * 2;
+  };
+}
+
+const setPropsOnFunc = (func) => {
+  func.year = '20??';
+  func.divideByTwo = (num) => {
+    return num / 2;
+  };
+};
+
+const shallowCopy = function (arg1, arg2) {
+  if (Array.isArray(arg1)) {
+    return arg1.concat(arg2);
+  } else {
+    return { ...arg1, ...arg2 };
+  }
+};

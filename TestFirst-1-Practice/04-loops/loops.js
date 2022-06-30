da91017680aed4080480f276f08ea4297781247b
Corum Gold <goldcorum@gmail.com>
2022-06-30 17:22:18 -0400

    Solve grid generator -- 2nd

--- TestFirst-1-Practice/04-loops/loops.js
+++ TestFirst-1-Practice/04-loops/loops.js
@@ -30,3 +30,31 @@ function join(arr, delimiter) {
     return joined.slice(0, joined.length - 1);
   } else return joined;
 }
+
+function gridGenerator(num) {
+  let grid = '';
+  for (let i = 0; i < num; i++) {
+    let row = '';
+    let rowNumber = i;
+    for (let j = 0; j < num; j++) {
+      //if it's an even row...
+      if (rowNumber % 2 === 0) {
+        //start the line with a hash
+        if (j % 2 === 0) {
+          row += '#';
+        } else row += ' ';
+        //if it's an odd row...
+      } else {
+        //start the line with a space
+        if (j % 2 === 0) {
+          row += ' ';
+        } else row += '#';
+      }
+    }
+    //at the end of the row, add a new line..
+    row += '\n';
+    //...and push it to the grid
+    grid += row;
+  }
+  return grid;
+}

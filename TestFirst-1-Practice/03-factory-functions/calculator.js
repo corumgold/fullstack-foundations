8c47a6d2701459e563fd2e9fca9691828665f188
Corum Gold <goldcorum@gmail.com>
2022-06-30 14:51:15 -0400

    Solve calculator -- 2nd

--- TestFirst-1-Practice/03-factory-functions/calculator.js
+++ TestFirst-1-Practice/03-factory-functions/calculator.js
@@ -1,1 +1,53 @@
 /* eslint-disable no-unused-vars */
+//solveCount = 1;
+
+const createCalculator = () => {
+  const calc = {
+    total: 0,
+    value() {
+      return this.total;
+    },
+    add(num) {
+      return (this.total += num);
+    },
+    subtract(num) {
+      return (this.total -= num);
+    },
+    clear() {
+      this.total = 0;
+    },
+  };
+  return calc;
+};
+
+function addSquareMethod(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let calculator = arr[i];
+    calculator.square = function () {
+      return this.total * this.total;
+    };
+  }
+  return arr;
+}
+
+const calculator = {
+  value() {
+    return this.total;
+  },
+  add(num) {
+    return (this.total += num);
+  },
+  subtract(num) {
+    return (this.total -= num);
+  },
+  clear() {
+    this.total = -10;
+  },
+};
+
+function createHumanCalculator() {
+    const calc = Object.create(calculator);
+    calc.total = -10;
+    return calc;
+}
+

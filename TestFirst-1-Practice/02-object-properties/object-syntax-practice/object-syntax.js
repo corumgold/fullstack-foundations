c5ed4d5d6a19b92865644a88295215d3f19fd7ea
Corum Gold <goldcorum@gmail.com>
2022-06-29 20:58:41 -0400

    Finish object syntax exercise -- 2nd

--- TestFirst-1-Practice/02-object-properties/object-syntax-practice/object-syntax.js
+++ TestFirst-1-Practice/02-object-properties/object-syntax-practice/object-syntax.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+let solveCount = 2;
+
+let loud = 'loud';
+let name = 'Cody';
+let dog = {
+  name,
+  bark() {
+    return 'ruff ruff!';
+  },
+  [`${loud}Bark`]: function () {
+    return this.bark().toUpperCase();
+  },
+};

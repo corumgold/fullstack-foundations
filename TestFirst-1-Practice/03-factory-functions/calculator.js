79d4ec81fa5de73384d10c6e6494a2b2c5091e65
Corum Gold <goldcorum@gmail.com>
2022-07-01 17:42:54 -0400

    Finish creatCalculator function exercise -- 3rd

--- TestFirst-1-Practice/03-factory-functions/calculator.js
+++ TestFirst-1-Practice/03-factory-functions/calculator.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 //solveCount = 2;
 
-const createCalculator = () => {
-  const calc = {
+function createCalculator() {
+  let calculator = {
     total: 0,
     value() {
       return this.total;
@@ -17,37 +17,5 @@ const createCalculator = () => {
       this.total = 0;
     },
   };
-  return calc;
-};
-
-function addSquareMethod(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let calculator = arr[i];
-    calculator.square = function () {
-      return this.total * this.total;
-    };
-  }
-  return arr;
+  return calculator;
 }
-
-const calculator = {
-  value() {
-    return this.total;
-  },
-  add(num) {
-    return (this.total += num);
-  },
-  subtract(num) {
-    return (this.total -= num);
-  },
-  clear() {
-    this.total = -10;
-  },
-};
-
-function createHumanCalculator() {
-    const calc = Object.create(calculator);
-    calc.total = -10;
-    return calc;
-}
-

f0105b2dd75f02a7397a65658cfe40867ca3e64c
Corum Gold <goldcorum@gmail.com>
2022-07-04 20:41:27 -0400

    Finish Pokemon constructor function -- 2nd

--- TestFirst-2-Practice/05-constructor-functions/simulate-battle/simulateBattle.js
+++ TestFirst-2-Practice/05-constructor-functions/simulate-battle/simulateBattle.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars, no-throw-literal*/
+//solveCount = 1;
+
+function Pokemon(name, health, attackBonus) {
+  this.name = name;
+  this.health = health;
+  this.attackBonus = attackBonus;
+}
+
+Pokemon.prototype.isDefeated = function () {
+  if (this.health > 0) return false;
+  return true;
+};
+
+Pokemon.prototype.biteAttack = function () {
+  return this.attackBonus + 2;
+};

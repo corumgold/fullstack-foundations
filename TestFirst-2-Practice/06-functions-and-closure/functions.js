13642c03e6edd4c870f7d11effce55dceb62508e
Corum Gold <goldcorum@gmail.com>
2022-07-07 11:22:28 -0400

    Finish function and closre -- 2nd

--- TestFirst-2-Practice/06-functions-and-closure/functions.js
+++ TestFirst-2-Practice/06-functions-and-closure/functions.js
@@ -1,1 +1,58 @@
 /* eslint-disable no-unused-vars */
+
+//solveCount = 2;
+
+function concatString() {
+  let strings = Array.from(arguments);
+  return strings.join('');
+}
+
+function yourFunctionRunner() {
+  let funcs = Array.from(arguments);
+  return funcs.map((func) => func()).join('');
+}
+
+function makeAdder(num) {
+  return (addNum) => num + addNum;
+}
+
+function once(func) {
+  let ran = false;
+  return () => {
+    if (!ran) {
+      ran = true;
+      return func();
+    } else return 'the function has already been called...';
+  };
+}
+
+function createObjectWithClosures() {
+  let total = 0;
+  let obj = {
+    oneIncrementer() {
+      total += 1;
+    },
+    tensIncrementer() {
+      total += 10;
+    },
+    setValue(num) {
+      total = num;
+    },
+    getValue() {
+      return total;
+    },
+  };
+  return obj;
+}
+
+function dontSpillTheBeans(message) {
+  let secret = message;
+  return {
+    getSecret() {
+      return secret;
+    },
+    setSecret(newSecret) {
+      secret = newSecret;
+    },
+  };
+}

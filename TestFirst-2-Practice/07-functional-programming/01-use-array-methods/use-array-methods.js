7c9c112df92b1a6cec5e700f9b8db33085ae551a
Corum Gold <goldcorum@gmail.com>
2022-07-08 15:36:29 -0400

    Finish using reduce exercises -- 2nd

--- TestFirst-2-Practice/07-functional-programming/01-use-array-methods/use-array-methods.js
+++ TestFirst-2-Practice/07-functional-programming/01-use-array-methods/use-array-methods.js
@@ -29,3 +29,45 @@ function swapCase(str) {
 function extensionSearch(ext, arr) {
   return arr.filter((word) => word.includes(ext));
 }
+
+//USING REDUCE
+
+function getPopulation(list, arr) {
+  return list
+    .filter((countryObj) => {
+      if (arr.length > 0) return arr.includes(countryObj.name);
+      else return countryObj;
+    })
+    .reduce((acc, curr) => {
+      return (acc += curr.population);
+    }, 0);
+}
+
+function keyifyArrayOfObjects(key, arr) {
+  return arr.reduce((acc, curr) => {
+    acc[curr[key]] = curr;
+    return acc;
+  }, {});
+}
+
+function powerLevelAverage(arr) {
+  return Math.floor(
+    arr.reduce((acc, curr) => {
+      return acc + curr.powerLevel;
+    }, 0) / arr.length
+  );
+}
+
+function mapReduce(arr, func) {
+  return arr.reduce((acc, curr) => {
+    acc.push(func(curr));
+    return acc;
+  }, []);
+}
+
+function filterReduce(arr, func) {
+  return arr.reduce((acc, curr) => {
+    if (func(curr)) acc.push(curr);
+    return acc;
+  }, []);
+}

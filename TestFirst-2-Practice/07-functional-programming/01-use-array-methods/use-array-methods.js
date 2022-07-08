33fca980534fc048b87dd5c9bb99ed4f33ea4388
Corum Gold <goldcorum@gmail.com>
2022-07-08 14:24:39 -0400

    Finish using map exercises -- 2nd

--- TestFirst-2-Practice/07-functional-programming/01-use-array-methods/use-array-methods.js
+++ TestFirst-2-Practice/07-functional-programming/01-use-array-methods/use-array-methods.js
@@ -1,1 +1,25 @@
 /* eslint-disable no-unused-vars */
+//solveCount = 1;
+
+//USING MAP
+
+function indexAndValue(arr) {
+  return arr.map((num) => {
+    return {
+      index: arr.indexOf(num),
+      value: num,
+    };
+  });
+}
+
+const capitalize = (str) => str.toUpperCase();
+
+function swapCase(str) {
+  return str
+    .split(' ')
+    .map((word, index) => {
+      if (index % 2 === 0) return capitalize(word);
+      else return word;
+    })
+    .join(' ');
+}

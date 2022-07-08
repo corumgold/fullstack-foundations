c0c02584ae232a90f4940d4fcf38cb740311789a
Corum Gold <goldcorum@gmail.com>
2022-07-08 12:44:47 -0400

    Finish recreate array methods -- 2nd

--- TestFirst-2-Practice/07-functional-programming/00-recreate-array-methods/functional.js
+++ TestFirst-2-Practice/07-functional-programming/00-recreate-array-methods/functional.js
@@ -1,1 +1,76 @@
 /* eslint-disable no-unused-vars */
+//solveCount = 2;
+
+function forEach(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+const doubler = (num) => num * 2;
+
+const map = function (arr, func) {
+  let funcedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    funcedArr.push(func(arr[i]));
+  }
+  return funcedArr;
+};
+
+const filter = function (arr, func) {
+  let filtered = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) filtered.push(arr[i]);
+  }
+  return filtered;
+};
+
+function includes(obj, element) {
+  if (Array.isArray(obj)) {
+    for (let i = 0; i < obj.length; i++) {
+      if (obj[i] === element) return true;
+    }
+  } else {
+    for (let key in obj) {
+      if (key === element || obj[key] === element) return true;
+    }
+  }
+  return false;
+}
+
+function countWords(num, string) {
+  let wordCount = num;
+  for (let i = 0; i < string.split(' ').length; i++) {
+    wordCount++;
+  }
+  return wordCount;
+}
+
+function reduce(arr, start, func) {
+  let accumulator = start;
+  for (let i = 0; i < arr.length; i++) {
+    accumulator = func(accumulator, arr[i]);
+  }
+  return accumulator;
+}
+
+function sum(arr) {
+  return reduce(arr, 0, (acc, curr) => acc + curr);
+}
+
+function every(arr, func) {
+  return (
+    reduce(arr, 0, (acc, curr) => {
+      return acc + func(curr);
+    }) === arr.length
+  );
+}
+
+function some(arr, func) {
+  return (
+    reduce(arr, 0, (acc, curr) => {
+      return (acc += func(curr));
+    }) > 0
+  );
+}
+

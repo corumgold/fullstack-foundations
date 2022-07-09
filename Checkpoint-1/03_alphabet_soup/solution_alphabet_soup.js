baffa83bbf6f6671976acbfe48527aa330b207b9
Corum Gold <goldcorum@gmail.com>
2022-07-09 13:47:39 -0400

    Finish alphabet soup -- 2nd

--- Checkpoint-1/03_alphabet_soup/solution_alphabet_soup.js
+++ Checkpoint-1/03_alphabet_soup/solution_alphabet_soup.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
-//Good Job!!!
+//solveCount = 2;
+
 function soup(phrase, bank) {
-  let bankArr = bank.split('');
+  const letters = bank.split('');
   for (let i = 0; i < phrase.length; i++) {
-    let letter = phrase[i];
-    if (bankArr.indexOf(letter) !== -1) {
-      bankArr.splice(bankArr.indexOf(letter), 1);
+    if (letters.includes(phrase[i])) {
+      letters.splice(letters.indexOf(phrase[i]), 1);
     } else return false;
   }
   return true;
 }
+

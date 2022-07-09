c180e5f0833ab8712394d584cf7620a029a3ae51
Corum Gold <goldcorum@gmail.com>
2022-07-09 12:05:48 -0400

    Finish exercise 00 -- 2nd

--- Checkpoint-1/00_find_obj_properties/solution_find_obj_properties.js
+++ Checkpoint-1/00_find_obj_properties/solution_find_obj_properties.js
@@ -1,20 +1,21 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
-//MM: Good Job!!!
+//solveCount = 2;
+
 function findObjPropsHasOwn(obj) {
   let arr = [];
   for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
-      arr.push(`${key}`); //<--it is not necessary to use the '${}'
+      arr.push(key);
     }
   }
   return arr.join(', ');
 }
 
 function findObjKeys(obj) {
-  let arr = [];
-  let keys = Object.keys(obj);
-  for (let i = 0; i < keys.length; i++) {
-    arr.push(`${keys[i]}`); //<--it is not necessary to use the '${}'
-  }
-  return arr.join(', ');
+  return Object.keys(obj)
+    .reduce((acc, curr) => {
+      acc.push(curr);
+      return acc;
+    }, [])
+    .join(', ');
 }

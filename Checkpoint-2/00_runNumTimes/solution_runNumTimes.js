37b0b4c62ef75a92a9b2b70617736b7f426981a2
Corum Gold <goldcorum@gmail.com>
2022-07-14 10:33:09 -0400

    Finish num times -- 2nd

--- Checkpoint-2/00_runNumTimes/solution_runNumTimes.js
+++ Checkpoint-2/00_runNumTimes/solution_runNumTimes.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+//solveCount = 2;
+
+function runNumTimes(func, times) {
+  let funced = [];
+  for (let i = 0; i < times; i++) {
+    funced.push(func());
+  }
+  return funced;
+}

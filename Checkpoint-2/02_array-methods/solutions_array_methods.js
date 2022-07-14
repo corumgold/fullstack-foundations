17408abfd20b8217ebdc608849beb676b292a0d1
Corum Gold <goldcorum@gmail.com>
2022-07-14 10:42:15 -0400

    Finish array methods

--- Checkpoint-2/02_array-methods/solutions_array_methods.js
+++ Checkpoint-2/02_array-methods/solutions_array_methods.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars */
+//solveCount = 2;
+
+function useMapToUpperCase(str) {
+  return str.split(' ').map((word) => word.toUpperCase());
+}
+
+function useFilter(arr) {
+  return arr.filter((element) => element.includes('@'));
+}
+
+function sumWithReduce(arr, start) {
+  if (!start) start = 0;
+  return arr.reduce((acc, curr) => acc + curr, start);
+}
+
+function filterEvensDoubleAndSum(arr) {
+  return arr
+    .filter((num) => num % 2 === 0)
+    .map((num) => num * 2)
+    .reduce((acc, curr) => acc + curr, 0);
+}

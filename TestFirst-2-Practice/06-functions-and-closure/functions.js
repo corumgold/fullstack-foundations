1fff7a4aec1310686fadd37182688a21774af6cf
Corum Gold <goldcorum@gmail.com>
2022-07-14 10:17:38 -0400

    Add checkpoint 2 file

--- TestFirst-2-Practice/06-functions-and-closure/functions.js
+++ TestFirst-2-Practice/06-functions-and-closure/functions.js
@@ -3,56 +3,27 @@
 //solveCount = 2;
 
 function concatString() {
-  let strings = Array.from(arguments);
-  return strings.join('');
+  let args = Array.from(arguments);
+  return args.join('');
 }
 
 function yourFunctionRunner() {
-  let funcs = Array.from(arguments);
+  let funcs = [...arguments];
   return funcs.map((func) => func()).join('');
 }
 
 function makeAdder(num) {
-  return (addNum) => num + addNum;
+  return function (num2) {
+    return num + num2;
+  };
 }
 
 function once(func) {
   let ran = false;
-  return () => {
+  return function () {
     if (!ran) {
       ran = true;
       return func();
     } else return 'the function has already been called...';
-  };
-}
-
-function createObjectWithClosures() {
-  let total = 0;
-  let obj = {
-    oneIncrementer() {
-      total += 1;
-    },
-    tensIncrementer() {
-      total += 10;
-    },
-    setValue(num) {
-      total = num;
-    },
-    getValue() {
-      return total;
-    },
-  };
-  return obj;
-}
-
-function dontSpillTheBeans(message) {
-  let secret = message;
-  return {
-    getSecret() {
-      return secret;
-    },
-    setSecret(newSecret) {
-      secret = newSecret;
-    },
   };
 }

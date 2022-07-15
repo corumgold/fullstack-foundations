9ed14b4ebd6ffc865135ab81ff2c8b6240fc8c83
Corum Gold <goldcorum@gmail.com>
2022-07-15 14:52:01 -0400

    Finish thrice -- 2nd

--- Checkpoint-2/03_thrice/solution_thrice.js
+++ Checkpoint-2/03_thrice/solution_thrice.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+//solveCount = 2;
+
+function thrice(func) {
+  let callCount = 0;
+  return function () {
+    if (callCount < 3) {
+      callCount++;
+      return func();
+    }
+  };
+}

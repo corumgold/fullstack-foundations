b4b6d2bb197b873ea2730400eb44fec34f43b963
Corum Gold <goldcorum@gmail.com>
2022-07-15 15:03:57 -0400

    Finish keypad -- 2nd

--- Checkpoint-2/04_keypad/solution_keypad.js
+++ Checkpoint-2/04_keypad/solution_keypad.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars */
+
+function presses(str) {
+  const PRESSES = {
+    1: '1adgjmptw ',
+    2: 'behknqux',
+    3: 'cfilorvy',
+    4: '23456s8z',
+    5: '79',
+  };
+
+  return str
+    .toLowerCase()
+    .split('')
+    .reduce((acc, curr) => {
+      for (let number in PRESSES) {
+        if (PRESSES[number].includes(curr)) {
+          return acc + parseInt(number);
+        }
+      }
+    }, 0);
+}

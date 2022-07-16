4066298b48279b3ea270aedce5ad4fa96c6b35fe
Corum Gold <goldcorum@gmail.com>
2022-07-16 16:41:58 -0400

    Finish array prototype -- 2nd

--- Review-Practice/classes/01-array-prototype/array-prototype.js
+++ Review-Practice/classes/01-array-prototype/array-prototype.js
@@ -1,1 +1,24 @@
 /* eslint-disable no-unused-vars */
+//solveCount = 2;
+Array.prototype.maap = function (func) {
+  let maapped = [];
+  this.forEach((element) => maapped.push(func(element)));
+  return maapped;
+};
+
+Array.prototype.fiilter = function (func) {
+  let fiiltered = [];
+  this.forEach((element) => {
+    if (func(element)) fiiltered.push(element);
+  });
+  return fiiltered;
+};
+
+Array.prototype.reeduce = function (func, start) {
+  if (!start) start = 0;
+  let reeduced = start;
+  this.forEach((element) => {
+    reeduced += element;
+  });
+  return reeduced;
+};

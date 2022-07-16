eccd0ffd13444a75cd74d1949fbda61b634bcc72
Corum Gold <goldcorum@gmail.com>
2022-07-16 12:35:47 -0400

    Finish rename files -- 2nd

--- Review-Practice/basics/03-rename-files/rename-files.js
+++ Review-Practice/basics/03-rename-files/rename-files.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+
+//solveCount = 2;
+
+function renameFiles(arr) {
+    return arr.reduce((acc, curr) => {
+        if (!acc.includes(curr)) acc.push(curr);
+        else {
+            let incrementer = 1
+            while(acc.includes(`${curr}(${incrementer})`)) {
+                incrementer++
+            }
+            acc.push(`${curr}(${incrementer})`)
+        }
+        return acc;
+    }, [])
+}

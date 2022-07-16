7f668b781a666bd1ae643e82fbd1ffe830d8ffbb
Corum Gold <goldcorum@gmail.com>
2022-07-16 15:19:55 -0400

    Finish mapQuest -- 2nd

--- Review-Practice/basics/04-map-quest/solution-map-quest.js
+++ Review-Practice/basics/04-map-quest/solution-map-quest.js
@@ -1,1 +1,16 @@
 /* eslint-disable no-unused-vars */
+
+//solveCount = 2;
+
+function mapQuest(arr) {
+  const PAIRS = { N: 'S', S: 'N', E: 'W', W: 'E' };
+  return arr.reduce((acc, curr) => {
+    if (acc[acc.length - 1] === PAIRS[curr]) {
+      acc.pop();
+      return acc;
+    } else {
+      acc.push(curr);
+      return acc;
+    }
+  }, []);
+}

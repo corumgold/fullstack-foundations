41c68783e29c9feaf57ca08349a88e7c92724f8b
Corum Gold <goldcorum@gmail.com>
2022-07-15 20:45:16 -0400

    Finish pigify -- 2nd

--- Review-Practice/basics/01-pig-latin/pig-latin.js
+++ Review-Practice/basics/01-pig-latin/pig-latin.js
@@ -1,1 +1,19 @@
 /* eslint-disable no-unused-vars */
+
+//solveCount = 2;
+
+function pigify(str) {
+    const VOWELS = 'aeio'
+
+    return str.split(' ').map(word => {
+        debugger
+        let firstVowelIdx;
+        for (let char of word) {
+            if (VOWELS.includes(char)) {
+                firstVowelIdx = word.indexOf(char);
+                break;
+            }
+        }
+        return word.slice(firstVowelIdx) + word.slice(0, firstVowelIdx) + 'ay'
+    }).join(' ')
+}

006dd05cf974561ac2137ef54ded4381d00bede9
Corum Gold <goldcorum@gmail.com>
2022-07-16 15:49:07 -0400

    Solve multipleMaps -- 2nd

--- Review-Practice/basics/05-multiple-maps/multiple-maps.js
+++ Review-Practice/basics/05-multiple-maps/multiple-maps.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+
+// solveCount = 2;
+
+function multipleMaps(arr, arrObj) {
+  return arr.map((index) => {
+    let returnVal;
+    arrObj.forEach((obj) => {
+      if (Object.keys(obj).includes(`${index}`)) returnVal = obj[index];
+    });
+    return returnVal;
+  });
+}

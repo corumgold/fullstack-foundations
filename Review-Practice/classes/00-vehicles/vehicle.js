9ae8e22c41c673e359b245f31a7a1c32fb16c82c
Corum Gold <goldcorum@gmail.com>
2022-07-16 16:26:46 -0400

    Finish vehicle -- 2nd

--- Review-Practice/classes/00-vehicles/vehicle.js
+++ Review-Practice/classes/00-vehicles/vehicle.js
@@ -1,1 +1,30 @@
 /* eslint-disable no-unused-vars */
+//solveCount = 2;
+
+class Vehicle {
+  constructor(arr) {
+    this.wheelTotal = arr[0];
+    this.energySource = arr[1];
+    this.manufacturer = arr[2];
+    this.isOn = false;
+  }
+  getSpec(key) {
+    if (this.hasOwnProperty(key)) {
+      return this[key];
+    }
+  }
+}
+
+class Tesla extends Vehicle {
+  constructor(arr) {
+    super(arr[4][0]);
+    this.milesPerCharge = arr[0];
+    this.model = arr[1];
+    this.chargePercentage = arr[2];
+    this.minutesToCharge = arr[3];
+  }
+
+  minutesToFullCharge(minutes) {
+    return `${minutes} minutes until the charge is full!`
+  }
+}

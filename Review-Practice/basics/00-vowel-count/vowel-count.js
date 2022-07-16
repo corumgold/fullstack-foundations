5d25ca7b06c162588b61c99512361931ca79c5c7
Corum Gold <goldcorum@gmail.com>
2022-07-15 20:30:45 -0400

    Finish vowel count -- 2nd

--- Review-Practice/basics/00-vowel-count/vowel-count.js
+++ Review-Practice/basics/00-vowel-count/vowel-count.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+
+//solveCount = 2;
+
+function vowelsCount(str) {
+    return str.toLowerCase().split('').reduce((acc, curr) => {
+        if (Object.keys(acc).includes(curr)) {
+            acc[curr]++;
+            acc['total']++;
+        }
+        return acc;
+    }, {a: 0, e: 0, i: 0, o: 0, u: 0, total: 0})
+}

327a3cb0e51d894d3fc6da95ee5b6aed1d3bf96b
Corum Gold <goldcorum@gmail.com>
2022-07-17 11:56:18 -0400

    Finish rotater -- 2nd

--- Review-Practice/closure/03-rotater/rotater.js
+++ Review-Practice/closure/03-rotater/rotater.js
@@ -1,1 +1,19 @@
 /* eslint-disable no-unused-vars */
+
+// let solveCount = 2;
+
+function rotater(str) {
+  let direction = 'left';
+  return function (num) {
+    if (num === str.length) {
+      if (direction === 'left') direction = 'right';
+      else direction = 'left';
+      return str;
+    }
+    if (direction === 'left') {
+      return str.slice(num) + str.slice(0, num);
+    } else {
+      return str.slice(str.length - num) + str.substring(0, str.length - num);
+    }
+  };
+}

0bc75f216b0a421baa64d5e91337b6e879007069
Corum Gold <goldcorum@gmail.com>
2022-07-17 12:18:09 -0400

    Finish do not include -- 2nd

--- Review-Practice/this-keyword-context/01-do-not-include/do-not-include.js
+++ Review-Practice/this-keyword-context/01-do-not-include/do-not-include.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-unused-vars, no-extend-native */
+
+//solveCount = 2;
+
+Array.prototype.doNotInclude = function (args) {
+  if (!Array.isArray(args)) args = [args];
+  return this.filter((element) => !args.includes(this.indexOf(element)));
+};

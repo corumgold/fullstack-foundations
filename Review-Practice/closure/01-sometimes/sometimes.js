4d3e0214580032ae1f0dcc329201ee35b72d9c5e
Corum Gold <goldcorum@gmail.com>
2022-07-17 10:44:13 -0400

    Finish sometimes

--- Review-Practice/closure/01-sometimes/sometimes.js
+++ Review-Practice/closure/01-sometimes/sometimes.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+
+//solveCount = 2;
+
+function sometimes(func) {
+  let runCt = 0;
+  return function (...arg) {
+    runCt++;
+    if (runCt < 4 || runCt % 2 === 1) {
+      return func(...arg);
+    } else return 'I do not know!';
+  };
+}

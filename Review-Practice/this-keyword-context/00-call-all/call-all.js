1417d6ff3396a555cbbbd7093803bdf7abf73f57
Corum Gold <goldcorum@gmail.com>
2022-07-17 12:04:37 -0400

    Finish call all -- 2nd

--- Review-Practice/this-keyword-context/00-call-all/call-all.js
+++ Review-Practice/this-keyword-context/00-call-all/call-all.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+
+//solveCount = 2;
+
+function callAll(obj, funcArr) {
+  return funcArr.reduce((acc, curr) => {
+    acc.push(curr.call(obj));
+    return acc;
+  }, []);
+}

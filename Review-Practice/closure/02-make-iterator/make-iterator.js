7e9edd2fd58683aaddc7cf982ef922c72189de2b
Corum Gold <goldcorum@gmail.com>
2022-07-17 11:02:14 -0400

    Finish make iterator -- 2nd

--- Review-Practice/closure/02-make-iterator/make-iterator.js
+++ Review-Practice/closure/02-make-iterator/make-iterator.js
@@ -1,1 +1,20 @@
 /* eslint-disable no-unused-vars */
+
+//solveCount = 2;
+
+function makeIterator(arr) {
+  let currIdx = 0;
+  return {
+    getIndex() {
+      return currIdx;
+    },
+    getNext() {
+      currIdx++;
+      let next = {
+        value: arr[currIdx - 1],
+      };
+      currIdx > arr.length ? (next.done = true) : (next.done = false);
+      return next;
+    },
+  };
+}

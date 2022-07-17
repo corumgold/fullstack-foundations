6f2978fa67acac3977ecd938f9c5476bfe46f305
Corum Gold <goldcorum@gmail.com>
2022-07-17 10:18:33 -0400

    Finish my event emitter -- 2nd

--- Review-Practice/classes/03-event-emitter/event-emitter.js
+++ Review-Practice/classes/03-event-emitter/event-emitter.js
@@ -1,2 +1,18 @@
 /* eslint-disable no-unused-vars */
 
+//solveCount = 2;
+
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  addListener(event, cbkFunc) {
+    if (this.events[event]) this.events[event].push(cbkFunc);
+    else this.events[event] = [cbkFunc];
+  }
+
+  emit(event, ...args) {
+    this.events[event].forEach((func) => func(...args));
+  }
+}

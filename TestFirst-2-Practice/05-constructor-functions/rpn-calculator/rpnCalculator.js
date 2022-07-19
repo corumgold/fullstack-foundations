0a52061f159b40c606dcda741f1e446a63e97d88
Corum Gold <goldcorum@gmail.com>
2022-07-19 12:42:40 -0400

    Finish rpn calculator -- 3rd

--- TestFirst-2-Practice/05-constructor-functions/rpn-calculator/rpnCalculator.js
+++ TestFirst-2-Practice/05-constructor-functions/rpn-calculator/rpnCalculator.js
@@ -1,69 +1,40 @@
 /* eslint-disable no-unused-vars, no-throw-literal*/
-//solveCount = 2;
+//solveCount = 3;
 
-function RPNCalculator() {
-  this.stack = [];
-  this.push = function (num) {
-    this.stack.push(num);
-  };
+class RPNCalculator {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(val) {
+    this.stack.push(val);
+  }
+
+  value() {
+    return this.stack[0];
+  }
+
+  plus() {
+    if (this.stack.length > 1) {
+      this.stack.push(this.stack.shift() + this.stack.shift());
+    } else throw 'rpnCalculatorInstance is empty';
+  }
+
+  minus() {
+    if (this.stack.length > 1) {
+      this.stack.push(this.stack.shift() - this.stack.shift());
+    } else throw 'rpnCalculatorInstance is empty';
+  }
+
+  times() {
+    if (this.stack.length > 1) {
+      this.stack.push(this.stack.shift() * this.stack.shift());
+    } else throw 'rpnCalculatorInstance is empty';
+  }
+
+  divide() {
+    if (this.stack.length > 1) {
+      this.stack.push(this.stack.shift() / this.stack.shift());
+    } else throw 'rpnCalculatorInstance is empty';
+  }
 }
-
-RPNCalculator.prototype.value = function () {
-  return this.stack[0];
-};
-
-RPNCalculator.prototype.plus = function () {
-  let sum = 0;
-  if (this.stack.length < 2) {
-    throw 'rpnCalculatorInstance is empty';
-  } else {
-    for (let i = 0; i < this.stack.length; i++) {
-      sum += this.stack[i];
-      this.stack.shift();
-      i--;
-    }
-    this.stack.push(sum);
-  }
-};
-
-RPNCalculator.prototype.minus = function () {
-  if (this.stack.length < 2) {
-    throw 'rpnCalculatorInstance is empty';
-  } else {
-    let initial = this.stack.shift();
-    for (let i = 0; i < this.stack.length; i++) {
-      initial -= this.stack[i];
-      this.stack.shift();
-      i--;
-    }
-    this.stack.push(initial);
-  }
-};
-
-RPNCalculator.prototype.times = function () {
-  if (this.stack.length < 2) {
-    throw 'rpnCalculatorInstance is empty';
-  } else {
-    let product = 1;
-    for (let i = 0; i < this.stack.length; i++) {
-      product *= this.stack[i];
-      this.stack.shift();
-      i--;
-    }
-    this.stack.push(product);
-  }
-};
-
-RPNCalculator.prototype.divide = function () {
-  if (this.stack.length < 2) {
-    throw 'rpnCalculatorInstance is empty';
-  } else {
-    let divisor = this.stack.shift();
-    for (let i = 0; i < this.stack.length; i++) {
-      divisor /= this.stack[i];
-      this.stack.shift();
-      i--;
-    }
-    this.stack.push(divisor);
-  }
-};

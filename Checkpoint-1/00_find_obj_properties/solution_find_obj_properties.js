f079975256935329cde1e0432393293896567304
Corum Gold <goldcorum@gmail.com>
2022-07-21 22:05:40 -0500

    Finish find obj properties -- 3rd

--- Checkpoint-1/00_find_obj_properties/solution_find_obj_properties.js
+++ Checkpoint-1/00_find_obj_properties/solution_find_obj_properties.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
-//solveCount = 2;
+//solveCount = 3;
 
 function findObjPropsHasOwn(obj) {
-  let arr = [];
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      arr.push(key);
-    }
-  }
-  return arr.join(', ');
+  return Object.keys(obj)
+    .reduce((acc, curr) => {
+      if (obj.hasOwnProperty(curr)) acc.push(curr);
+      return acc;
+    }, [])
+    .join(', ');
 }
 
 function findObjKeys(obj) {
